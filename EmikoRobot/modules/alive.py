24554f450f7a8706b74314bb714671004d355d6f
BionXP <112658830+BionXP@users.noreply.github.com>
2022-10-05 19:29:31 +0800

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,16 +9,16 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/d73f4f0bb371f265b8d14.jpg"
+PHOTO = "https://telegra.ph/file/edaf091ed55c7f3eba3e4.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Zoids Robot.** \n\n"
-  TEXT += "💌 **I'm Working Properly** \n\n"
-  TEXT += f"💌 **My Master : [Xavier](https:)** \n\n"
-  TEXT += f"💌 **Library Version :** `{telever}` \n\n"
-  TEXT += f"💌 **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"💌 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Xavier Robot.** \n\n"
+  TEXT += " **I'm Working Properly** \n\n"
+  TEXT += f"🔰**My Master : [Xavier](https://t.me/Papih404)** \n\n"
+  TEXT += f"🔰**Library Version :** `{telever}` \n\n"
+  TEXT += f"🔰**Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"🔰**Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ✨**"
-  BUTTON = [[Button.url("Help", "https://t.me/CielRobot?start=help"), Button.url("Support", "https://t.me/ZoidsXRobotlog")]]
+  BUTTON = [[Button.url("Help", "https://t.me/zaxviermusicbot?start=help"), Button.url("Support", "https://t.me/ZoidsXRobotlog")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)

9d919b36b5881850b3ff0db837814d11d2b41f0e
BionXP <112658830+BionXP@users.noreply.github.com>
2022-10-06 23:43:12 +0800

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -422,14 +422,14 @@ def emiko_about_callback(update, context):
         )
     elif query.data == "emiko_support":
         query.message.edit_text(
-            text="*๏ Emiko support chats*"
+            text="*๏ Zaxvier support chats*"
             f"\nJoin My Support Group/Channel for see or report a problem on {dispatcher.bot.first_name}.",
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="Support", url="t.me/ZoidXRobotlog"),
-                    InlineKeyboardButton(text="Updates", url="https://t.me/ZoidXRobotlog"),
+                    InlineKeyboardButton(text="Support", url="t.me/zaxviersupport"),
+                    InlineKeyboardButton(text="Updates", url="https://t.me/zaxviersupport"),
                  ],
                  [
                     InlineKeyboardButton(text="Go Back", callback_data="emiko_"),
